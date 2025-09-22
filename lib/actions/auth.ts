a88bb9856df5f48bf08a5b593134c8184d72a4d5
mimprog <mimprog2@gmail.com>
2025-09-22 11:27:18 +0100

    qstash workflows onboarding api route edited

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -55,7 +55,7 @@ export const signUp = async (params:AuthCredentials) => {
         console.log('inserted successfully', insertedUser);
 
         await workflowClient.trigger({
-            url: `${config.env.prodApiEndpoint}/api/workflow/onboarding`,
+            url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
             body: {
                 email,
                 fullName
